5a3a75debfd5cc3981feb79f4b9b935d1e52f3f5
Aliyare Warsame <caliyare712@gmail.com>
2022-09-19 23:14:10 -0700

    mandatory

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+ in range(ord('a'), ord('z') + 1):
+         if chr(i) == 'e' or chr(i) == 'q':
+                     continue
+                     else:
+                                 print("{:s}".format(chr(i)), end="")
