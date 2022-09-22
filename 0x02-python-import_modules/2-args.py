82b5c9e8eba2348014955102ad1a791d610dab23
Aliyare Warsame <caliyare712@gmail.com>
2022-09-22 06:38:34 -0700

    advanced

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,9 +10,9 @@ def print_arg(argv):
         else:
             print("{:d} arguments:".format(n))
             i = 1
-            while i <= n:
-                print("{:d}: {:s}".format(i, argv[i]))
-                i += 1
+        while i <= n:
+            print("{:d}: {:s}".format(i, argv[i]))
+            i += 1
 if __name__ == "__main__":
     import sys
     print_arg(sys.argv)
